edfa467c3ebadb5337dbd8911d8029057b5bdaaf
Lav <lavhuyaar@gmail.com>
2025-04-16 16:51:37 +0530

    feat: Prisma models created

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    title: 'Hello world',
+  });
+});
 
 //Global error handler
 app.use(errorHandler);
